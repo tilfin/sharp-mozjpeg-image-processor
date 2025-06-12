c8234fbc9204b703a0d20e5bbb58764e903ee4fe
Toshimitsu Takahashi <toshi@tilfin.com>
2025-06-12 23:40:45 +0900

    add ESM test script for image processing functionality

---
+++ test/index.test.mjs
@@ -0,0 +1,51 @@
+import assert from 'assert'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { ImageProcessor } from '../lib/index.mjs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const processor = new ImageProcessor()
+
+async function generateScaledImages(filePath) {
+  const srcStream = fs.createReadStream(filePath)
+  const imgInfos = [
+    {
+      kind: 'large',
+      width: 1200,
+      height: 1200,
+    },
+    {
+      kind: 'small',
+      width: 800,
+      height: 800,
+    },
+    {
+      kind: 'thumb',
+      width: 400,
+      height: 400,
+      crop: true,
+    }
+  ]
+  const quality = 70
+
+  const outImgInfos = await processor.execute(srcStream, imgInfos, quality)
+  for (let outImgInfo of outImgInfos) {
+    console.log(outImgInfo)
+  }
+}
+
+assert.doesNotReject(async () => {
+  const photoFilePath = path.join(__dirname, `test-photo.jpg`)
+  const photoFilePath1 = path.join(__dirname, `test1.temp.jpg`)
+  const photoFilePath2 = path.join(__dirname, `test2.temp.jpg`)
+  fs.copyFileSync(photoFilePath, photoFilePath1)
+  fs.copyFileSync(photoFilePath, photoFilePath2)
+
+  await Promise.all([
+    generateScaledImages(photoFilePath1),
+    generateScaledImages(photoFilePath2),
+  ])
+})
